80f3f90fb6fe36cc32e92b3de4e382c5f56c8409
GudmundurE <gudmundure19@ru.is>
2021-03-30 20:24:57 +0000

    classes.py

---
+++ classes.py
@@ -0,0 +1,21 @@
+def Warrior():
+
+    health = 200
+    dmg = 30
+    luck = 2
+    return health, dmg, luck
+
+def Ranger():
+
+    health = 200
+    dmg = 30
+    luck = 2
+    return health, dmg, luck
+
+def Mage():
+
+    health = 200
+    dmg = 30
+    luck = 2
+
+    return health, dmg, luck
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import classes
+
 print("Welcome")
 wants_to_play = input("Press 1 to start playing!   ")
 if wants_to_play == "1":
@@ -5,13 +7,15 @@ if wants_to_play == "1":
      print("You play as a dwarf who is on his way to a bar")
      class_chosen = input("Chose your class: Warrior/1, Ranger/2, Mage/3   ")
      if class_chosen == "1":
-            health, damage, luck = Warrior()
+            health, damage, luck = classes.Warrior()
             
      elif class_chosen == "2":
-            health, damage, luck = Ranger()
+            health, damage, luck = classes.Ranger()
                 
      elif class_chosen == "3":
-            health, damage, luck = Mage()
+            health, damage, luck = classes.Mage()
                     
      else:
          print("bæ")
+
+print(health)
