4a0344d4211a5045dba709ad1ca25bd00f449e4f
pallisve <pallandri99@gmail.com>
2021-03-31 18:20:20 +0000

    Bætti vopnum við

--- main.py
+++ main.py
@@ -14,22 +14,46 @@ while True:
               
               if class_chosen == "1":
                      hp, dmg, luck = classes.Warrior()
+
+                     weapon_chosen = input("Choose your weapon: Axe/1, Sword/2: ")
+                     if weapon_chosen == "1":
+                            dmg_mod, hit_chance = classes.Axe()
+                     elif weapon_chosen == "2":
+                            dmg_mod, hit_chance = classes.Sword()
                      break
               
               elif class_chosen == "2":
                      hp, dmg, luck = classes.Ranger()
+
+                     weapon_chosen = input("Choose your weapon: Bow/1, Crossbow/2: ")
+                     if weapon_chosen == "1":
+                            dmg_mod, hit_chance = classes.Bow()
+                     elif weapon_chosen == "2":
+                            dmg_mod, hit_chance = classes.Crossbow()       
+                     
                      break
                      
               elif class_chosen == "3":
                      hp, dmg, luck = classes.Mage()
+
+                     weapon_chosen = input("Choose your weapon: Firestaff/1, Frostaff/2: ")
+                     if weapon_chosen == "1":
+                            dmg_mod, hit_chance = classes.Firestaff()
+                     elif weapon_chosen == "2":
+                            dmg_mod, hit_chance = classes.Froststaff()
+
                      break
               else:
                      class_chosen = input("Choose a class by selecting 1, 2 or 3: ")
+
+              
                      
        else:   
               wants_to_play = input("Press 1 to start playing!: ")  
 
-print("You start with ", hp, " hp, ", dmg, "dmg and ", luck, "luck")
+
+
+print("You start with ", hp, " hp, ", dmg, "dmg and ", luck, "luck", hit_chance, dmg_mod)
 left_or_right = input("First choice... Left or Right (Left = 1 / Right = 2)?")
 while left_or_right != "1" or "2":
               if left_or_right == "1":
